9771154f781e59b8a9284045a0bf078d2046b3c0
Liu <a_liu@tss-hr420-dm21.fc.ca>
2018-09-26 11:35:19 -0400

    updates

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+//refenerce the required packages to make task running work
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+  // place code for your default task here
+  return gulp.src('./sass/**/*.scss')
+  .pipe(sass({outputStyle : 'compressed'}).on('error', sass.logError))
+  ,pipe(gulp.dest('./css'));
+});
